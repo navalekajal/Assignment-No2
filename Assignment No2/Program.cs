bacdd264619356cf855c29a60bdffc6b5fde1b09
navalekajal <113415224+navalekajal@users.noreply.github.com>
2022-09-20 21:34:39 +0530

    Add project files.

---
+++ Assignment No2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Assignment_No2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter a mark math subject");
+//            int s1 = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Enter a mark physics subject");
+//            int s2 = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Enter a mark chemistery subject");
+//            int s3 = Convert.ToInt32(Console.ReadLine());
+
+//            int total = s1 + s2 + s3;
+//            Console.WriteLine($" Total is {total}");
+
+//            int per = total / 3;
+//            Console.WriteLine($" percentage is {per}");
+
+//            if (per >= 80)
+//            {
+//                Console.WriteLine("student in Grade A");
+//            }
+//            else if (per <= 80 && per >= 70)
+//            {
+//                Console.WriteLine("student in Grade B");
+//            }
+//            else if (per <= 70 && per >= 60)
+//            {
+//                Console.WriteLine("student in Grade C");
+//            }
+//            else if (per < 60)
+//            {
+//                Console.WriteLine("student in Grade D");
+
+
+//            }
+//        }
+//    }
+//}
---
+++ Assignment No2/program2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment_No2
+{
+    internal class program2
+    {
+        static void Main(string[] args)
+        {
+            int DA, HRA, gross;
+            Console.WriteLine("Enter a Basic salary");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+
+            if (salary <= 10000)
+            {
+                DA = (salary * 80) / 100;
+                HRA = (salary * 20) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+            }
+
+            else if (salary <= 20000)
+            {
+                DA = (salary * 90) / 100;
+                HRA = (salary * 35) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+
+            }
+            else 
+            {
+                DA = (salary * 95) / 100;
+                HRA = (salary * 40) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+
+            }
+        }
+    }
+}
+
